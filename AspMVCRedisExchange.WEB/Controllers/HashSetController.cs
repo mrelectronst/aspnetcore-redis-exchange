4c54e6cc3c75c6831eab3d82dad2afbc4ead0bcd
Fatih Karaman <fatih.karaman.dn@aygaz.com.tr>
2022-06-09 14:52:46 +0300

    Added HashSet Data Type

---
+++ AspMVCRedisExchange.WEB/Controllers/HashSetController.cs
@@ -0,0 +1,42 @@
+﻿using AspMVCRedisExchange.WEB.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspMVCRedisExchange.WEB.Controllers
+{
+    public class HashSetController : BaseController
+    {
+        public string listKey = "books";
+
+        public HashSetController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            HashSet<string> bookList = new HashSet<string>();
+
+            if(_database.KeyExists(listKey))
+                _database.SetMembers(listKey).ToList().ForEach(x => bookList.Add(x));
+
+            return View(bookList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string book)
+        {
+            if (!_database.KeyExists(listKey))
+                _database.KeyExpireAsync(listKey, DateTime.Now.AddMinutes(5)).Wait();
+
+            _database.SetAddAsync(listKey, book).Wait();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(string book)
+        {
+            await _database.SetRemoveAsync(listKey, book);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
