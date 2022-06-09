096e3845161de84cfb1041575d0a2e912e7c6631
Fatih Karaman <fatih.karaman.dn@aygaz.com.tr>
2022-06-09 12:08:04 +0300

    Add project files.

---
+++ AspMVCRedisExchange.WEB/Controllers/StringController.cs
@@ -0,0 +1,43 @@
+﻿using AspMVCRedisExchange.WEB.Services;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace AspMVCRedisExchange.WEB.Controllers
+{
+    public class StringController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        private readonly IDatabase _database;
+
+        public StringController(RedisService redisService)
+        {
+            _redisService = redisService;
+
+            _redisService.Connect();
+
+            _database = _redisService.GetDatabase(0);
+        }
+
+        public IActionResult Index()
+        {
+            _database.StringSet("name", "fth");
+
+            _database.StringSet("age", "10");
+
+            return View();
+        }
+
+
+        public IActionResult ShowString()
+        {
+            _database.StringIncrement("age");
+
+            ViewBag.Name = _database.StringGet("age");
+
+
+            return View();
+        }
+
+    }
+}
---
+++ AspMVCRedisExchange.WEB/Services/RedisService.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+
+namespace AspMVCRedisExchange.WEB.Services
+{
+    public class RedisService
+    {
+        private ConnectionMultiplexer multiplexer;
+
+        private readonly string redisHost;
+
+        private readonly string redisPort;
+
+        public RedisService(IConfiguration configuration)
+        {
+            redisHost = configuration["Redis:Host"];
+
+            redisPort = configuration["Redis:Port"];
+        }
+
+        public void Connect()
+        {
+            var configString = $"{redisHost}:{redisPort}";
+
+            multiplexer = ConnectionMultiplexer.Connect(configString);
+        }
+
+        public IDatabase GetDatabase(int database)
+        {
+            return multiplexer.GetDatabase(database);
+        }
+    }
+}
